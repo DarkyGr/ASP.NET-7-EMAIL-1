55c63b7cac8a6da5c5c4db1014062bf1e976b46b
Guillermo Riaño <guilledark17@gmail.com>
2023-03-27 17:03:44 -0600

    ✅ Adding Service: EmailService

--- ASP.NET_7_SendEmail/Models/EmailDTO.cs
+++ ASP.NET_7_SendEmail/Models/EmailDTO.cs
@@ -4,7 +4,7 @@
     {
         public string To { get; set; } = string.Empty;
 
-        public string Issue { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
 
         public string Body { get; set; } = string.Empty;
     }
---
+++ ASP.NET_7_SendEmail/Services/EmailService.cs
@@ -0,0 +1,55 @@
+﻿using ASP.NET_7_SendEmail.Models;
+using MailKit.Security;
+using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+
+namespace ASP.NET_7_SendEmail.Services
+{
+    public class EmailService : IEmailService
+    {
+        // Adding the configuration of appsettings
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void SendEmail(EmailDTO request)
+        {
+            var email = new MimeMessage();
+
+            // From
+            email.From.Add(MailboxAddress.Parse(_configuration.GetSection("Email:UserName").Value));
+
+            // To
+            email.To.Add(MailboxAddress.Parse(request.To));
+
+            // Subject
+            email.Subject = request.Subject;
+
+            // Body
+            email.Body = new TextPart(TextFormat.Html) { 
+                Text = request.Body
+            };
+
+            // Configuration SMTP
+            using var smtp = new SmtpClient();
+            smtp.Connect(
+                _configuration.GetSection("Email:Host").Value,
+                Convert.ToInt32(_configuration.GetSection("Email:Port").Value),
+                SecureSocketOptions.StartTls
+            );
+
+            // Adding Autenticate of the email
+            smtp.Authenticate(_configuration.GetSection("Email:UserName").Value, _configuration.GetSection("Email:Password").Value);
+
+            // Send Emial
+            smtp.Send(email);
+
+            // Disconect of Server
+            smtp.Disconnect(true);
+        }
+    }
+}

ebc4f8fd20df5960e969d63f65c30b28c5571d77
Guillermo Riaño <guilledark17@gmail.com>
2023-03-27 17:07:26 -0600

    📈 Adding Service to Program.cs

--- ASP.NET_7_SendEmail/Program.cs
+++ ASP.NET_7_SendEmail/Program.cs
@@ -1,3 +1,5 @@
+using ASP.NET_7_SendEmail.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Adding Service
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 var app = builder.Build();
 

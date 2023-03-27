1fc2d306b65441863c4ae203c09f03116f72a49e
Guillermo Riaño <guilledark17@gmail.com>
2023-03-27 17:13:34 -0600

    ✅ Adding Controller: Email

---
+++ ASP.NET_7_SendEmail/Controllers/EmailController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ASP.NET_7_SendEmail.Services;
+using ASP.NET_7_SendEmail.Models;
+
+namespace ASP.NET_7_SendEmail.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        // Method to Send Email
+        [HttpPost]
+        public IActionResult SendEmail(EmailDTO request)
+        {
+            _emailService.SendEmail(request);
+            return Ok();
+        }
+    }
+}
